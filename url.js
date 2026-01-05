72f09be4f4885312830fdafcbbba1e02501576b7
Reagan-Cheng <54108719+Reagan-Cheng@users.noreply.github.com>
2026-01-05 23:37:47 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,10 +1,4 @@
 const urls = [
-	'https://www.surveycake.com/s/rpdy7',
-	'https://www.surveycake.com/s/14wgK',
-	'https://www.surveycake.com/s/opMd9',
-	'https://www.surveycake.com/s/vpgZe',
-	'https://www.surveycake.com/s/blMpl',
-	'https://www.surveycake.com/s/LxBP7',
-	'https://www.surveycake.com/s/qp6Z9',
-	'https://www.surveycake.com/s/kpM69',
+	'https://www.surveycake.com/s/4oz76',
+	'https://www.surveycake.com/s/Lxv42',
 ];
