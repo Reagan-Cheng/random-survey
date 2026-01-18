e0a5a0083703f9f1aa54505e78477891e9da72ae
Reagan-Cheng <54108719+Reagan-Cheng@users.noreply.github.com>
2026-01-18 23:23:38 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,10 +1,4 @@
 const urls = [
-	'https://www.surveycake.com/s/enrAa',
-	'https://www.surveycake.com/s/Yqye7',
-	'https://www.surveycake.com/s/9XgLd',
-	'https://www.surveycake.com/s/OYxqa',
-	'https://www.surveycake.com/s/bl08g',
-	'https://www.surveycake.com/s/4og86',
-	'https://www.surveycake.com/s/LxrD2',
-	'https://www.surveycake.com/s/33g7R',
+	'https://www.surveycake.com/s/rpKNv',
+	'https://www.surveycake.com/s/2LV04',
 ];
